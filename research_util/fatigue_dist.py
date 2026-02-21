293ddce924d69cc43b2f45869be28a98f97dae17
Gwilym-Rutherford <gmarutherford1@sheffield.ac.uk>
2026-02-21 12:14:01 +0000

    test uniform resarch util

--- research_util/fatigue_dist.py
+++ research_util/fatigue_dist.py
@@ -1,6 +1,7 @@
 from src.patient_data_dispatcher import PatientDataDispatcher, PatientDataType
 from src.core.enums import MileStone
 
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -12,7 +13,7 @@ for milestone in MileStone:
 
     pdd = PatientDataDispatcher("config/config.yaml", [], milestone)
     metadata = pdd.get_patient_data(PatientDataType.META)
-    print(metadata)
+    # print(metadata)
     fatigue_values = metadata["MFISTO1N"].tolist()
     fatigue_values = list(filter(lambda x: x > 0, fatigue_values))
 
@@ -21,8 +22,12 @@ for milestone in MileStone:
 print(f"number of values: {len(all_fatigue_values)}")
 print(f"max: {np.max(all_fatigue_values)}")
 print(f"min: {np.min(all_fatigue_values)}")
-plt.hist(all_fatigue_values)
+
+normal = all_fatigue_values
+uniform = stats.rankdata(all_fatigue_values) / len(all_fatigue_values) * 84
+
+plt.hist(uniform)
 plt.ylabel("Frequency")
 plt.xlabel("Fatigue values")
-plt.savefig(os.path.join(os.getcwd(), "research_util", "fatigue_distribution"))
+plt.savefig(os.path.join(os.getcwd(), "research_util", "fatigue_distribution_uniform"))
 plt.show()

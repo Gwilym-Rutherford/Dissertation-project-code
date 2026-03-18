a60e9648e59dbf78165b45977d3b3ad07c6145f1
Gwilym-Rutherford <gmarutherford1@sheffield.ac.uk>
2026-03-18 13:18:40 +0000

    split data into training and testing

--- sanity_check.py
+++ sanity_check.py
@@ -2,6 +2,9 @@ from src.model import DMOLSTM
 from src.model import sanity
 from src.logger import ExperimentLogger
 from src.evaluation import Evaluation
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 import pandas as pd
 import numpy as np
@@ -9,100 +12,67 @@ import os
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# import dataset
 df = pd.read_csv(
-    os.path.join(os.getcwd(), "data/sanity_check/household_power_consumption.txt"),
-    sep=";",
+    os.path.join(os.getcwd(), "data/sanity_check/beijing_sanity_check.csv"),
+    sep=",",
     low_memory=False,
 )
+df = df.drop(columns=["Iws", "Is", "Ir"])
 
-df.replace("?", np.nan, inplace=True)
-df.dropna(inplace=True)
+# wind direction encoding
+encoder = LabelEncoder()
+df["cbwd"] = encoder.fit_transform(df["cbwd"])
 
-numerical_features = df.iloc[:, 2:]
-features = numerical_features.to_numpy(dtype=np.float32)[:10000]
+# split into days and data to fit impute
+days = {}
 
-WINDOW_SIZE = 5
+for index, row in df.iterrows():
+    year_month_day = f"{row['year']}{row['month']}{row['day']}"
+    if year_month_day not in days:
+        days[year_month_day] = []
 
-train_index = np.floor(len(features) * 0.8).astype(np.int32)
+    days[year_month_day].append(
+        torch.tensor([row["pm2.5"], row["DEWP"], row["TEMP"], row["PRES"], row["cbwd"]])
+    )
 
-logger = ExperimentLogger(sanity)
+# split into input and label data
+input_data = []
+label_data = []
 
-training_data = torch.tensor(features[:train_index, :]).to(device=device)
-test_data = torch.tensor(features[train_index:, :]).to(device=device)
+for _, value in days.items():
+    day_tensor = torch.stack(value)
 
-model = DMOLSTM(sanity).to(device=device)
-optimiser = sanity.optimiser(model.parameters(), lr=sanity.learning_rate)
+    input_data.append(day_tensor[:-1])
+    label_data.append(day_tensor[-1])
 
-for epoch in range(sanity.epochs):
-    # train loop
-    model.train()
-    avg_loss = []
-    for window in range(len(training_data) - WINDOW_SIZE - 1):
-        data_in_window = training_data[window:window + WINDOW_SIZE, :]
-        label = training_data[window + WINDOW_SIZE + 1, :]
-        
-        data_in_window = torch.tensor(data_in_window).unsqueeze(dim=0)
-        label = torch.tensor(label)
+input_data = torch.stack(input_data)
+label_data = torch.stack(label_data)
 
-        optimiser.zero_grad()
-        pred = model(data_in_window).squeeze(dim=0)
+# split data into training and testing data
+training_split = 0.8
+testing_split = 0.2
 
-        loss = sanity.loss_fn(pred, label)
-        avg_loss.append(loss.item())
-        
-        loss.backward()
-        optimiser.step()
+n_samples = len(label_data)
+training_index = np.floor(n_samples * training_split).astype(np.int64)
 
-    avg_loss = np.array(avg_loss)
-    avg_loss_val = np.mean(avg_loss)
-    # print(avg_loss_val)
-    logger.log_values([("training_loss", avg_loss_val)])
+training_input = input_data[0:training_index]
+training_label = label_data[0:training_index]
 
-    # validation loop
-    model.eval()
-    validation_avg_loss = []
-    with torch.no_grad():
-        for window in range(len(test_data) - WINDOW_SIZE - 1):
-            data_in_window = training_data[window:window + WINDOW_SIZE, :]
-            label = training_data[window + WINDOW_SIZE + 1, :]
-            
-            data_in_window = torch.tensor(data_in_window).unsqueeze(dim=0)
-            label = torch.tensor(label)
+testing_input = input_data[training_index:]
+testing_label = label_data[training_index:]
 
-            optimiser.zero_grad()
-            pred = model(data_in_window).squeeze(dim=0)
+# impute missing data only fitting on training data
+impute = IterativeImputer(random_state=0, missing_values=np.nan)
+x, y, z = training_input.shape
+train_input_2d = torch.reshape(training_input, (x * y, z))
+train_imputed_2d = impute.fit_transform(train_input_2d)
+training_input = torch.tensor(train_imputed_2d).view(x, y, z)
 
-            loss = sanity.loss_fn(pred, label)
-            validation_avg_loss.append(loss.item())
+x, y, z = testing_input.shape
+testing_input_2d = torch.reshape(testing_input, (x * y, z))
+testing_imputed_2d = impute.transform(testing_input_2d)
+testing_input = torch.tensor(testing_input_2d).view(x, y, z)
 
-        validation_avg_loss = np.array(validation_avg_loss)
-        validation_loss_val = np.mean(validation_avg_loss)
-        print(validation_loss_val)
-        logger.log_values([("validation_loss", validation_loss_val)])
-
-
-# final test loop
-model.eval()
-pred_list = []
-labels_list = []
-with torch.no_grad():
-    for window in range(len(test_data) - WINDOW_SIZE - 1):
-        data_in_window = training_data[window:window + WINDOW_SIZE, :]
-        label = training_data[window + WINDOW_SIZE + 1, :]
-        
-        data_in_window = torch.tensor(data_in_window).unsqueeze(dim=0)
-        label = torch.tensor(label)
-
-        optimiser.zero_grad()
-        pred = model(data_in_window).squeeze(dim=0)
-
-        pred_list.append(pred)
-        labels_list.append(label)
-
-    pred = torch.cat(pred_list).cpu()
-    labels = torch.cat(labels_list).cpu()
-
-    evaluation = Evaluation(pred, labels)
-
-    logger.save(evaluation, show_fig=False)
-    
+# Remove all input and respected label if the label == NaN
